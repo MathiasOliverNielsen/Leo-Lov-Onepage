7b032736ce9f6d08c4615a44bb5df8dc6e4ee4b0
MathiasOliverNielsen <math324e@elev.techcollege.dk>
2026-01-08 14:06:29 +0100

    Services, created and basic styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Hero from "./components/Hero/Hero.tsx";
 import Navbar from "./components/Navbar/Navbar.tsx";
+import Services from "./components/Services/Services.tsx";
 
 function App() {
   return (
     <>
       <Hero />
       <Navbar />
+      <Services />
     </>
   );
 }
