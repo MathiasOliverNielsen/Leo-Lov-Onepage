a7d9fb7ad7e28e11d4c6f70e13445fd3b179715b
MathiasOliverNielsen <math324e@elev.techcollege.dk>
2026-01-09 09:39:08 +0100

    Services component, and styleing done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar.tsx";
 import Hero from "./components/Hero/Hero.tsx";
+import Services from "./components/Services/Services.tsx";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Services />
     </>
   );
 }
